76b9772162a489534be5fc3c5d6f085a103b16f7
davmer1878 <david.merriles@sky.com>
2022-05-12 19:29:08 +0100

    Update branch my-course-work

--- src/main/java/guru/springframework/spring5recipeapp/controller/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controller/IndexController.java
@@ -1,14 +1,38 @@
 package guru.springframework.spring5recipeapp.controller;
 
+import guru.springframework.spring5recipeapp.repository.CategoryRepository;
+import guru.springframework.spring5recipeapp.repository.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController
 {
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository)
+    {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "index", "index.html"})
     public String showIndex()
     {
+        categoryRepository.findByDescription("Mexican").ifPresentOrElse(
+                category -> System.out.println("Category Id: " + category.getId()),
+                () -> System.out.println("Category does not exist"));
+        categoryRepository.findByDescription("Invalid").ifPresentOrElse(
+                category -> System.out.println("Category Id: " + category.getId()),
+                () -> System.out.println("Category does not exist"));
+        unitOfMeasureRepository.findByDescription("Pinch").ifPresentOrElse(
+                unitOfMeasure -> System.out.println("Unit Of Measure Id: " + unitOfMeasure.getId()),
+                () -> System.out.println("Unit of Measure does not exist"));
+        unitOfMeasureRepository.findByDescription("Punch").ifPresentOrElse(
+                unitOfMeasure -> System.out.println("Unit Of Measure Id: " + unitOfMeasure.getId()),
+                () -> System.out.println("Unit of Measure does not exist"));
+
         return "index";
     }
 }
